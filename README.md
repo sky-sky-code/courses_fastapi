4a983c6561ff62bd5dfa405823285b3e28dd7ac7
Сайдулла Пашаев <saidyllapashaev@mail.ru>
2024-02-21 12:47:01 +0300

    нагрузочное тестирование и locustfile

---
+++ README.md
@@ -0,0 +1,10 @@
+# Courses
+_______
+
+* для получения цены валютных пар указать в query запросе нужно их указать через - (/courses&symbols=BTC-USDT)
+* multy query запрос (/courses&symbols=BTC-USDT&symbols=ETH-BTC&symbols=BTC-RUB)
+
+
+## Нагрузочное тестирование
+________
+![1](./locustmedia/locust.png)
--- courses/locustfile.py
+++ courses/locustfile.py
@@ -4,11 +4,15 @@ from locust import HttpUser, TaskSet, task
 class UserBehavior(TaskSet):
     @task
     def get_courses(self):
-        self.client.get("http://127.0.0.1:8000/courses?symbol=BTCRUB")
+        for symbol in ['BTC-RUB', 'ETH-BTC', 'BTC-USDT']:
+            self.client.get(f'/courses?symbols={symbol}')
+
+    @task
+    def get_multi_courses(self):
+        self.client.get('/courses?symbols=LTC-BTC&symbols=BNB-BTC&symbols=NEO-BTC&symbols=QTUM-ETH&symbols=EOS-ETH')
 
 
 class WebsiteUser(HttpUser):
     tasks = [UserBehavior]
     min_wait = 1000
     max_wait = 1000
-
